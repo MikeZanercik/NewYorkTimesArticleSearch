090e436e650aa5c35c1a14a45b6a9a5ebd3e0d74
cbush19 <cbush5400@gmail.com>
2019-10-30 14:04:16 -0400

    update queryURL

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,14 @@ var APIKey = "aO1xoU3JZcF2hwh8eKAfVKwwn2Err6aw";
 
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=" + APIKey;
 
+var apiKey = "&api-key=17gQQhZPuy5FP49ABMFG71G76XgGmq2u";
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=";
+var searchWord = "election";
+
+queryURL += searchWord;
+queryURL += apiKey;
+
+
 $.ajax({
     url: queryURL,
     method: "GET"
