69c28fb0d15abc6909ac316bc3dc1ecfc4532013
Randall Phillips <rphill45@uncc.edu>
2019-10-30 14:05:17 -0400

    added query variables

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var APIKey = "aO1xoU3JZcF2hwh8eKAfVKwwn2Err6aw";
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=" + APIKey;
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&api-key=" + APIKey;
 
 $.ajax({
     url: queryURL,
