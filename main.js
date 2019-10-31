72a7c2356a750eee41ab99f861e8fefba05f59a4
cbush19 <cbush5400@gmail.com>
2019-10-31 11:03:18 -0400

    added start year and end year to query

--- main.js
+++ main.js
@@ -3,9 +3,17 @@ $(".btn-primary").on("click", function () {
   event.preventDefault();
   searchWord = $(":input.form-control").val()
   searchNumber = $(":input#numberSelector").val()
+  searchStartYear = "&begin_date="+$("#startYear").val()+'0101'
+  searchEndYear = "&end_date="+ $("#endYear").val()+'1231'
+  
+console.log(searchStartYear);
+console.log(searchEndYear); 
 
 var APIKey = "aO1xoU3JZcF2hwh8eKAfVKwwn2Err6aw";
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&api-key=" + APIKey;
+
+queryURL += searchStartYear + searchEndYear;
+console.log(queryURL);
 
 $.ajax({
   url: queryURL,
